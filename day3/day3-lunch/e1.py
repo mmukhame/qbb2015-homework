177cbab925673336b141bcecd47ab2f5b323344d
Maryam Mukhamedova <cmdb@BIOL-CMDB-23.local>
2015-08-26 13:24:34 -0400

    Exercise one completed

--- day3/day3-lunch/e1.py
+++ day3/day3-lunch/e1.py
@@ -14,9 +14,29 @@ for sample in metadata[metadata["sex"] == "female"]["sample"]:
 Sxlm=[]
 for sample in metadata[metadata["sex"] == "male"]["sample"]:
     df1=pd.read_table("~/qbb2015/stringtie/"+sample+"/t_data.ctab")
-    roi1=df["t_name"].str.contains("FBtr0331261") #grab the row interested in 
+    roi1=df["t_name"].str.contains("FBtr0331261") 
     Sxlm.append(df1[roi1]["FPKM"].values)
-#pass to plot with three commands
+
+metadata=pd.read_csv("~/qbb2015/rawdata/replicates.csv")
+Sx1=[]
+for sample in metadata[metadata["sex"] == "male"]["sample"]:
+    df2=pd.read_table("~/qbb2015/stringtie/"+sample+"/t_data.ctab")
+    roi2=df2["t_name"].str.contains("FBtr0331261") 
+    Sx1.append(df2[roi2]["FPKM"].values)
+    
+print Sx1
+#[14A, 0], [14B, 0], [14C, 0], [14D, 0])    
+Sx2=[]
+for sample in metadata[metadata["sex"] == "female"]["sample"]:
+    df2=pd.read_table("~/qbb2015/stringtie/"+sample+"/t_data.ctab")
+    roi2=df2["t_name"].str.contains("FBtr0331261") 
+    Sx2.append(df2[roi2]["FPKM"].values)
+    
+print Sx2
+
+#[14A, 0], [14B, 79.103477], [14C, 182.233475], [14D, 2.409474])
+
+    
 
 plt.figure()
 plt.title("Sxl")
@@ -24,10 +44,24 @@ plt.xlabel("developmental stage")
 plt.ylabel("mRNA Abundance (RPKM)")
 plt.plot(Sxl, color = 'r', label='female')
 plt.plot(Sxlm, color = 'b', label = 'male')
-plt.legend(['female', 'male'])
+plt.legend(['female', 'male'], loc='center left')
 xticks=['10', '11', '12', '13', '14A', '14B', '14C', '14D']
 plt.xticks (range(8), xticks)
-plt.yticks(range(0,350,50))
+
+plt.plot(4, 0, 'o', color='r')
+plt.plot(5, 79.103477, 'o', color ='r')
+plt.plot(6, 182.233475, 'o', color = 'r')
+plt.plot(7, 2.409474, 'o', color = 'r')
+
+
+plt.plot(4, 0, 'o', color='b')
+plt.plot(5, 0, 'o', color ='b')
+plt.plot(6, 0, 'o', color = 'b')
+plt.plot(7, 0, 'o', color = 'b')
 plt.savefig("Exercise1.png")
+
+plt.yticks(range(0,900,50))
+
+
 
 
