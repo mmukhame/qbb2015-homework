f3faeab34490082f14ea7bcc1410cf5ac9fb285b
Maryam Mukhamedova <cmdb@BIOL-CMDB-23.local>
2015-08-26 14:56:53 -0400

    Exercise three, added KDE

--- day3/day3-lunch/e3.py
+++ day3/day3-lunch/e3.py
@@ -4,17 +4,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 df = pd.read_table("~/qbb2015/stringtie/SRR072893/t_data.ctab")
 df2=df[df["FPKM"] > 0]["FPKM"]
 
 df3=np.log(df2)
 print df3
 
-df4=df3.values
-
 plt.figure()
-plt.hist(df4)
-plt.title("Histogram of FPKM values in SRR072893")
-plt.xlabel("log(FPKM values)")
-plt.savefig("histogram.png")
+df3.plot(kind='kde')
+plt.savefig("KDE.png")
+
