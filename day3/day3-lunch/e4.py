099d35f331aaf4f925d0a63b54b001b6cfeb850e
Maryam Mukhamedova <cmdb@BIOL-CMDB-23.local>
2015-08-27 10:01:55 -0400

    changed code in exercise four to confirm the same shape of two sets using for MA plot

--- day3/day3-lunch/e4.py
+++ day3/day3-lunch/e4.py
@@ -7,24 +7,31 @@ import numpy as np
 
 #data set1
 df = pd.read_table("~/qbb2015/stringtie/SRR072893/t_data.ctab")
-df2=df[df["FPKM"] > 0]["FPKM"]
-df3=np.log(df2).values
+df2=df["FPKM"] > 0
 
 
 #data set2
-df4=pd.read_csv("~/qbb2015/rawdata/samples.csv")
-df5=df[df["FPKM"] > 0]["FPKM"]
-df6=np.log(df5).values
+df4=pd.read_table("~/qbb2015/stringtie/SRR072893/t_data.ctab")
+df5=df4["FPKM"] >0
+
+b1=df2 & df5 #when they are both zero, a row will be true. Will be false when one of them or both are false. Ensures the same shape
+
+df3=df[b1]["FPKM"] 
+df6=df4[b1]["FPKM"]
+
+l1=np.log2(df3) #rows that are true in both 
+l2=np.log2(df6)
 
 
-M=df6-df5
-A=(df5+df6)/2
+M=l1-l2 #ratio of intensity (log based)
+A=(df5+df6)*0.5
 
 print M
 print A
 
 plt.figure()
 plt.scatter(M, A)
+plt.title("MA Plot")
 plt.ylabel("ratio")
 plt.xlabel("average")
 plt.savefig("MAplot.png")
